32eb743569c48fbbaa16c8cc435467b69786d935
SD-PARK <97375357+SD-PARK@users.noreply.github.com>
2024-02-13 21:52:16 +0900

    feat: UseGuards 추가

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/user.dto';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
 
 @Controller('user')
 export class UserController {
     constructor(private readonly userService: UserService) {}
 
     @Get()
+    @UseGuards(JwtAuthGuard)
     async getUserAll(): Promise<User[]> {
         return this.userService.getUserAll();
     }
