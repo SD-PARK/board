14332bba2f93ed89967a511d3966fc2b487e6396
SD-PARK <97375357+SD-PARK@users.noreply.github.com>
2024-02-13 19:05:26 +0900

    feat: ValidationPipe 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { validationPipeConfig } from './config/validation-pipe.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe(validationPipeConfig));
   await app.listen(3000);
 }
 bootstrap();
