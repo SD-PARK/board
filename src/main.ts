f144d65447a531780ead9eb162ef210a0242585e
SD-PARK <97375357+SD-PARK@users.noreply.github.com>
2024-02-14 12:07:38 +0900

    feat: cookie-parser 설치

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { validationPipeConfig } from './config/validation-pipe.config';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe(validationPipeConfig));
+  app.use(cookieParser());
   await app.listen(3000);
 }
 bootstrap();
