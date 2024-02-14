1b78e59d40037524949487fa8297250a3b3a7a70
SD-PARK <97375357+SD-PARK@users.noreply.github.com>
2024-02-14 23:48:47 +0900

    fix: CategoryModule import

--- src/board/board.module.ts
+++ src/board/board.module.ts
@@ -3,9 +3,13 @@ import { BoardController } from './board.controller';
 import { BoardService } from './board.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Board, ViewBoardList } from './entity/board.entity';
+import { CategoryModule } from 'src/category/category.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Board, ViewBoardList])],
+  imports: [
+    TypeOrmModule.forFeature([Board, ViewBoardList]),
+    CategoryModule,
+  ],
   controllers: [BoardController],
   providers: [BoardService],
   exports: [BoardService],
